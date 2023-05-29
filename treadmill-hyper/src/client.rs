04f3bad7bec0e2b2a1d29393a566dc7805a979cf
xd009642 <danielmckenna93@gmail.com>
2023-05-29 22:38:14 +0100

    This is why I can't have nice things

--- treadmill-hyper/src/client.rs
+++ treadmill-hyper/src/client.rs
@@ -2,11 +2,14 @@ use crate::*;
 use hyper::client::connect::{Connected, Connection};
 use hyper::service::Service;
 use hyper::Uri;
+use std::net::*;
 use std::{
     future::Future,
     pin::Pin,
     task::{self, Poll},
 };
+use trust_dns_resolver::config::*;
+use trust_dns_resolver::Resolver;
 
 impl Connection for TreadmillStream {
     fn connected(&self) -> Connected {
@@ -32,4 +35,15 @@ impl Service<Uri> for TreadmillConnector {
         // We need to do a DNS resolution and
         todo!();
     }
+}
+
+fn resolve_ip(uri: Uri) -> IpAddr {
+    let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+
+    // Lookup the IP addresses associated with a name.
+    // The final dot forces this to be an FQDN, otherwise the search rules as specified
+    //  in `ResolverOpts` will take effect. FQDN's are generally cheaper queries.
+    let response = resolver.lookup_ip("www.example.com.").unwrap();
+
+    response.iter().next().expect("no addresses returned!")
 }
